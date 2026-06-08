b4f5edc1848e0990446defc068d0ccd0e8149568
josee-arant <josee.arant@gmail.com>
2026-06-08 19:10:10 +0200

    Initial commit

---
+++ 2c/ej2c3_test.py
@@ -0,0 +1,16 @@
+import pytest
+from ej2c3 import factorial, calculate_factorial
+
+
+def test_factorial():
+    assert factorial(5) == 120, "factorial does not return the correct value for input 5. It should be 120"    
+    with pytest.raises(
+        ValueError, match="Factorial of a negative number cannot be calculated"
+    ):
+        factorial(-5)
+    with pytest.raises(
+        ValueError, match="Factorial of a negative number cannot be calculated"
+    ):
+        factorial(-1)
+    assert calculate_factorial(8) == 40320, "calculate_factorial does not return the correct value for input 8. It should be 40320"
+    assert calculate_factorial(-10).__contains__("An unexpected error has occurred:"), "calculate_factorial does not return the correct value for input -10. It should contain 'An unexpected error has occurred:'"
